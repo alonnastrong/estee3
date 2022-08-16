4bc957520fcfb2dc9e9edd57d31cd57973e09e50
alonnastrong <110996560+alonnastrong@users.noreply.github.com>
2022-08-16 16:40:36 -0400

    Add files via upload

---
+++ estee2/ViewController2.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController2.swift
+//  estee2
+//
+//  Created by Scholar on 8/16/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
